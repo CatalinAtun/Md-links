c0f7db52d3b5a2e085aab13951a405f845cc5e68
Dennisse Moreno C <catalina.camposcaceres9@gmail.com>
2019-02-26 17:17:28 -0300

    Función reconoce ruta relativa y la usa como absoluta

--- mdlinks.js
+++ mdlinks.js
@@ -1,31 +1,30 @@
-// función que dice el status de la página
-
-function run(){ 
-    fs.readdir(process.argv[2], function cb(err, data){ // readdir tira en un array las carpetas que hay
-      console.log((data))
-    })
-    }
-    run();
-
-
-    // ------------------------------------------------------------------------------
-    const chalk = require('chalk');
-
-    let fs = require('fs');
-    let markdownLinkExtractor = require('markdown-link-extractor');
-    let markdown = fs.readFileSync('README.md').toString();
-    let links = markdownLinkExtractor(markdown);
+  
+const chalk = require('chalk');
+let fs = require('fs');
+let markdownLinkExtractor = require('markdown-link-extractor');
+let markdown = fs.readFileSync('README.md').toString();
+let links = markdownLinkExtractor(markdown);  
+const path = require('path');
+const extension = path.extname(process.argv[2]);
+const fetch = require('node-fetch');
     
-    const path = require('path');
-    const extension = path.extname(process.argv[2]);
-    const fetch = require('node-fetch');
-    
-    links.forEach(function (link) {
-        if(extension === '.md'){
-                fetch(link).then((response)=>{    
+
+links.forEach(function (link) {
+    if(extension === '.md'){
+        fetch(link).then((response)=>{    
             if(response.statusText === "OK"){
                 console.log(chalk.cyan(link) + " " +chalk.black.bgGreen('Página funcionando'))}})
                 fetch(link).then((response)=>{
-                if(response.statusText === 404 || response.statusText === 'Not Found'){
-                    console.log(chalk.cyan(link)+" "+chalk.black.bgRed('Página fuera de servicio'))}})
-                }})
+                    if(response.statusText === 404 || response.statusText === 'Not Found'){
+                        console.log(chalk.cyan(link)+" "+chalk.black.bgRed('Página fuera de servicio'))}})
+                    }})
+                
+                
+
+fs.readdir(process.argv[2], 'utf8', (err, data)=>{
+    data.map(function(file){
+        console.log(path.join(process.argv[2], file));
+    })
+})
+
+
