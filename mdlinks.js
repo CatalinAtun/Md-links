1d90ca200234110a1e749f2e89d7dc8c2d355fd3
Dennisse Moreno C <catalina.camposcaceres9@gmail.com>
2019-02-27 17:44:02 -0300

    Archivo se convierte en modulo

--- mdlinks.js
+++ mdlinks.js
@@ -1,19 +1,21 @@
-//const chalk = require('chalk');
+const chalk = require('chalk');
 let fs = require('fs');
 let markdownLinkExtractor = require('markdown-link-extractor');
 let markdown = fs.readFileSync('README.md').toString();
 let links = markdownLinkExtractor(markdown);  
-const path = require('path');
-const extension = path.extname(process.argv[2]);
+const linkPath = require('path');
 const fetch = require('node-fetch');
 
-let absolutePath = path.resolve(process.argv[2]); //absolutePath sustituye a process.argv[2]
 
-const prom = new Promise((resolve, reject)=>{
+function mdLinks(path, options){ 
+    const absolutePath = linkPath.resolve(path);
+    const extension = linkPath.extname(path);
+
+    const prom = new Promise((resolve)=>{
     if(extension === '.md'){
         resolve(links.forEach(function (link) {
-            fetch(link).then((response)=>{    
-                if(response.statusText === "OK"){
+            fetch(link).then((response)=>{ 
+                    if(response.statusText === "OK"){
                     console.log({
                         "Link": link,
                         "Validation": "OK",
@@ -31,6 +33,15 @@ const prom = new Promise((resolve, reject)=>{
                             "Ruta": absolutePath
                         })
                     })
-                }))}})
-                    
-                    console.log(prom)
+                }))}
+                else{
+                    console.log("No es un archivo md")
+                }
+            })
+                    prom.then(console.log("Links encontrados: "))
+        }
+        
+    if(require.main === module) 
+        mdLinks(process.argv[2])
+
+    module.exports = mdLinks;
