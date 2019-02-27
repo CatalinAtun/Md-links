835ff0be9a7f9cc9079a5333bf6ba0b6f39ecc7a
Dennisse Moreno C <catalina.camposcaceres9@gmail.com>
2019-02-27 15:44:21 -0300

    Devuelve objeto con 3 propiedades

--- mdlinks.js
+++ mdlinks.js
@@ -1,5 +1,4 @@
-  
-const chalk = require('chalk');
+//const chalk = require('chalk');
 let fs = require('fs');
 let markdownLinkExtractor = require('markdown-link-extractor');
 let markdown = fs.readFileSync('README.md').toString();
@@ -7,24 +6,31 @@ let links = markdownLinkExtractor(markdown);
 const path = require('path');
 const extension = path.extname(process.argv[2]);
 const fetch = require('node-fetch');
-    
 
-links.forEach(function (link) {
+let absolutePath = path.resolve(process.argv[2]); //absolutePath sustituye a process.argv[2]
+
+const prom = new Promise((resolve, reject)=>{
     if(extension === '.md'){
-        fetch(link).then((response)=>{    
-            if(response.statusText === "OK"){
-                console.log(chalk.cyan(link) + " " +chalk.black.bgGreen('Página funcionando'))}})
-                fetch(link).then((response)=>{
-                    if(response.statusText === 404 || response.statusText === 'Not Found'){
-                        console.log(chalk.cyan(link)+" "+chalk.black.bgRed('Página fuera de servicio'))}})
-                    }})
-                
-                
-
-fs.readdir(process.argv[2], 'utf8', (err, data)=>{
-    data.map(function(file){
-        console.log(path.join(process.argv[2], file));
-    })
-})
-
-
+        resolve(links.forEach(function (link) {
+            fetch(link).then((response)=>{    
+                if(response.statusText === "OK"){
+                    console.log({
+                        "Link": link,
+                        "Validation": "OK",
+                        "Ruta": "ruta"
+                    })
+                }else{
+                    console.log({
+                        "Link": link,
+                        "Validation": "No funcionando",
+                        "Ruta": "ruta"
+                    })}}).catch(err=>{
+                        console.log({
+                            "Link": link,
+                            "Validation": "No funcionando",
+                            "Ruta": "ruta"
+                        })
+                    })
+                }))}})
+                    
+                    console.log(prom)
