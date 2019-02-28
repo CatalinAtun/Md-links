bdfca6a50b8c2c25dde340fe5b7e9e03784ab846
Dennisse Moreno C <catalina.camposcaceres9@gmail.com>
2019-02-28 18:22:40 -0300

    Se logra la recursividad. Aleluya

--- mdlinks.js
+++ mdlinks.js
@@ -1,4 +1,3 @@
-const chalk = require('chalk');
 let fs = require('fs');
 let markdownLinkExtractor = require('markdown-link-extractor');
 let markdown = fs.readFileSync('README.md').toString();
@@ -6,42 +5,50 @@ let links = markdownLinkExtractor(markdown);
 const linkPath = require('path');
 const fetch = require('node-fetch');
 
-
-function mdLinks(path, options){ 
+function mdLinks(path, options = {validate: false}){ 
     const absolutePath = linkPath.resolve(path);
     const extension = linkPath.extname(path);
-
     const prom = new Promise((resolve)=>{
     if(extension === '.md'){
         resolve(links.forEach(function (link) {
             fetch(link).then((response)=>{ 
                     if(response.statusText === "OK"){
                     console.log({
-                        "Link": link,
-                        "Validation": "OK",
-                        "Ruta": absolutePath
+                        "href": link,
+                        "text": "OK",
+                        "file": absolutePath
                     })
                 }else{
                     console.log({
-                        "Link": link,
-                        "Validation": "No funcionando",
-                        "Ruta": absolutePath
+                        "href": link,
+                        "text": "No funcionando",
+                        "file": absolutePath
                     })}}).catch(err=>{
                         console.log({
-                            "Link": link,
-                            "Validation": "No funcionando",
-                            "Ruta": absolutePath
+                            "href": link,
+                            "text": "No funcionando",
+                            "file": absolutePath
                         })
                     })
-                }))}
-                else{
-                    console.log("No es un archivo md")
+                }))
+            }else{ 
+                fs.lstat(path, (err, data)=>{
+                    //console.error(path);
+                    if(data.isDirectory() === true){
+                        fs.readdir(path, (err, dirContent)=>{
+                            const absolute = dirContent.map(element => linkPath.resolve(path, element))
+                            console.log(absolute)
+                            absolute.map(element =>{
+                                return mdLinks(element)
+                            })
+                        })
+                    }  
+                })
                 }
             })
-                    prom.then(console.log("Links encontrados: "))
+                    prom.then(console.log(" "))
         }
-        
+
     if(require.main === module) 
         mdLinks(process.argv[2])
-
     module.exports = mdLinks;
